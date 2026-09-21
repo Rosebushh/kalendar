f1fe0ca71d607384c6dc4394249ac2126512a5a4
Rosebushh <rosebushh@gmail.com>
2026-09-21 22:25:53 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // VIOLA KALENDÁŘ - Network-First Service Worker v9
-const CACHE_NAME = 'viola-kalendar-v12';
+const CACHE_NAME = 'viola-kalendar-v13';
 
 self.addEventListener('install', (event) => {
   self.skipWaiting();
